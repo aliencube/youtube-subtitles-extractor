915be05a3df23bbbeff707602ca6293ff408cc3d
Kim Seohyun <59790777+kshjessica@users.noreply.github.com>
2023-10-15 06:22:25 +0000

    feat: remove class VideoDetails properties

--- src/YouTubeSubtitlesExtractor/Models/VideoDetail.cs
+++ src/YouTubeSubtitlesExtractor/Models/VideoDetail.cs
@@ -5,18 +5,5 @@ namespace Aliencube.YouTubeSubtitlesExtractor.Models;
 /// </summary>
 public class VideoDetails
 {
-    /// <summary>
-    /// Gets or sets the video title.
-    /// </summary>
-    public string Title { get; set; }
-
-    /// <summary>
-    /// Gets or sets the video description.
-    /// </summary>
-    public string Description { get; set; }
-
-    /// <summary>
-    /// Gets or sets the list of available subtitle languages.
-    /// </summary>
-    public List<string> AvailableSubtitleLanguages { get; set; }
+    /// Forthcoming
 }
