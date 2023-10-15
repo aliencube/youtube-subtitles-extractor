b00d772a4fea6550108f5bf52da0a55b3102a757
Jin-Cheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2023-10-15 07:26:48 +0000

    add slash

--- src/YouTubeSubtitlesExtractor/YouTubeVideo.cs
+++ src/YouTubeSubtitlesExtractor/YouTubeVideo.cs
@@ -100,7 +100,7 @@ public class YouTubeVideo : IYouTubeVideo
         return subtitles;
     }
 
-    // <inheritdoc/>
+    /// <inheritdoc/>
     public async Task<VideoDetails> ExtractVideoDetailsAsync(string videoUrl)
     {
         throw new NotImplementedException();
