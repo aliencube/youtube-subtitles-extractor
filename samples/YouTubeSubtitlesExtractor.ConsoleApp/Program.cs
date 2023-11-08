398fc1dffd7660198c04df4fa19d1ad55b7b57a0
Justin Yoo <justin.yoojh@gmail.com>
2023-11-08 20:42:12 +0900

    Add console app as a sample

---
+++ samples/YouTubeSubtitlesExtractor.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Aliencube.YouTubeSubtitlesExtractor;
+using Aliencube.YouTubeSubtitlesExtractor.Abstractions;
+using Aliencube.YouTubeSubtitlesExtractor.ConsoleApp.Services;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+               .UseConsoleLifetime()
+               .ConfigureServices(services =>
+               {
+                   services.AddHttpClient();
+                   services.AddTransient<IYouTubeVideo, YouTubeVideo>();
+                   services.AddTransient<IYouTubeService, YouTubeService>();
+               })
+               .Build();
+
+var service = host.Services.GetRequiredService<IYouTubeService>();
+await service.ExecuteAsync(args);
