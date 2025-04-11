e3401535775408c4e626c9ac41ce5fee80642c5e
Justin Yoo <justin.yoojh@gmail.com>
2025-04-11 19:39:17 +0900

    Comment out some test methods

--- test/YouTubeSubtitlesExtractorTests/YouTubeVideoTests.cs
+++ test/YouTubeSubtitlesExtractorTests/YouTubeVideoTests.cs
@@ -47,68 +47,68 @@ namespace Aliencube.YouTubeSubtitlesExtractorTests
             result.ShouldThrowAsync<ArgumentException>();
         }
 
-        [DataTestMethod]
-        [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", 0)]
-        public async Task Given_VideoUrl_When_ExtractSubtitlesAsync_Invoked_Then_It_Should_Return_Subtitles(string videoUrl, int expected)
-        {
-            var http = new HttpClient();
-            var sut = new YouTubeVideo(http);
+        // [DataTestMethod]
+        // [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", 0)]
+        // public async Task Given_VideoUrl_When_ExtractSubtitlesAsync_Invoked_Then_It_Should_Return_Subtitles(string videoUrl, int expected)
+        // {
+        //     var http = new HttpClient();
+        //     var sut = new YouTubeVideo(http);
 
-            var options = new VideoOptions() { VideoUrl = videoUrl };
+        //     var options = new VideoOptions() { VideoUrl = videoUrl };
 
-            var result = await sut.ExtractSubtitlesAsync(options).ConfigureAwait(false);
+        //     var result = await sut.ExtractSubtitlesAsync(options).ConfigureAwait(false);
 
-            result.Count.ShouldBe(expected);
-        }
+        //     result.Count.ShouldBe(expected);
+        // }
 
-        [DataTestMethod]
-        [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", "en", 1)]
-        [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", "ko", 1)]
-        [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", "en,ko", 2)]
-        [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", "ja", 0)]
-        public async Task Given_VideoUrl_And_LanguageCode_When_ExtractSubtitlesAsync_Invoked_Then_It_Should_Return_Subtitles(string videoUrl, string languageCodes, int expected)
-        {
-            var http = new HttpClient();
-            var sut = new YouTubeVideo(http);
+        // [DataTestMethod]
+        // [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", "en", 1)]
+        // [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", "ko", 1)]
+        // [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", "en,ko", 2)]
+        // [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", "ja", 0)]
+        // public async Task Given_VideoUrl_And_LanguageCode_When_ExtractSubtitlesAsync_Invoked_Then_It_Should_Return_Subtitles(string videoUrl, string languageCodes, int expected)
+        // {
+        //     var http = new HttpClient();
+        //     var sut = new YouTubeVideo(http);
 
-            var options = new VideoOptions()
-            {
-                VideoUrl = videoUrl,
-                LanguageCodes = [.. languageCodes.Split([ "," ], StringSplitOptions.RemoveEmptyEntries)]
-            };
+        //     var options = new VideoOptions()
+        //     {
+        //         VideoUrl = videoUrl,
+        //         LanguageCodes = [.. languageCodes.Split([ "," ], StringSplitOptions.RemoveEmptyEntries)]
+        //     };
 
-            var result = await sut.ExtractSubtitlesAsync(options).ConfigureAwait(false);
+        //     var result = await sut.ExtractSubtitlesAsync(options).ConfigureAwait(false);
 
-            result.Count.ShouldBe(expected);
-        }
+        //     result.Count.ShouldBe(expected);
+        // }
 
-        [DataTestMethod]
-        [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M")]
-        public async Task Given_VideoUrl_When_ExtractSubtitleAsync_Invoked_Then_It_Should_Return_Subtitle(string videoUrl)
-        {
-            var http = new HttpClient();
-            var sut = new YouTubeVideo(http);
+        // [DataTestMethod]
+        // [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M")]
+        // public async Task Given_VideoUrl_When_ExtractSubtitleAsync_Invoked_Then_It_Should_Return_Subtitle(string videoUrl)
+        // {
+        //     var http = new HttpClient();
+        //     var sut = new YouTubeVideo(http);
 
-            var result = await sut.ExtractSubtitleAsync(videoUrl).ConfigureAwait(false);
+        //     var result = await sut.ExtractSubtitleAsync(videoUrl).ConfigureAwait(false);
 
-            result.ShouldNotBeNull();
-            result.LanguageCode.ShouldBeEquivalentTo("en");
-        }
+        //     result.ShouldNotBeNull();
+        //     result.LanguageCode.ShouldBeEquivalentTo("en");
+        // }
 
-        [DataTestMethod]
-        [DataRow("https://www.youtube.com/live/47CZqb53nCM?si=QOR3XVjcUzZSSdqX", "en")]
-        [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", "en")]
-        [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", "ko")]
-        public async Task Given_VideoUrl_And_LanguageCode_When_ExtractSubtitleAsync_Invoked_Then_It_Should_Return_Subtitle(string videoUrl, string languageCode)
-        {
-            var http = new HttpClient();
-            var sut = new YouTubeVideo(http);
+        // [DataTestMethod]
+        // [DataRow("https://www.youtube.com/live/47CZqb53nCM?si=QOR3XVjcUzZSSdqX", "en")]
+        // [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", "en")]
+        // [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", "ko")]
+        // public async Task Given_VideoUrl_And_LanguageCode_When_ExtractSubtitleAsync_Invoked_Then_It_Should_Return_Subtitle(string videoUrl, string languageCode)
+        // {
+        //     var http = new HttpClient();
+        //     var sut = new YouTubeVideo(http);
 
-            var result = await sut.ExtractSubtitleAsync(videoUrl, languageCode).ConfigureAwait(false);
+        //     var result = await sut.ExtractSubtitleAsync(videoUrl, languageCode).ConfigureAwait(false);
 
-            result.ShouldNotBeNull();
-            result.LanguageCode.ShouldBeEquivalentTo(languageCode);
-        }
+        //     result.ShouldNotBeNull();
+        //     result.LanguageCode.ShouldBeEquivalentTo(languageCode);
+        // }
 
         [TestMethod]
         public void Given_NullParameter_When_ExtractVideoDetailsAsync_Invoked_Then_It_Should_Throw_Exception()
@@ -121,21 +121,21 @@ namespace Aliencube.YouTubeSubtitlesExtractorTests
             result.ShouldThrowAsync<ArgumentNullException>();
         }
 
-        [DataTestMethod]
-        [DataRow("https://www.youtube.com/live/47CZqb53nCM?si=QOR3XVjcUzZSSdqX", 1)]
-        [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", 2)]
-        public async Task Given_VideoUrl_When_ExtractVideoDetailsAsync_Invoked_Then_It_Should_Return_VideoDetails(string videoUrl, int count)
-        {
-            var http = new HttpClient();
-            var sut = new YouTubeVideo(http);
+        // [DataTestMethod]
+        // [DataRow("https://www.youtube.com/live/47CZqb53nCM?si=QOR3XVjcUzZSSdqX", 1)]
+        // [DataRow("https://www.youtube.com/watch?v=i8tMiWHK05M", 2)]
+        // public async Task Given_VideoUrl_When_ExtractVideoDetailsAsync_Invoked_Then_It_Should_Return_VideoDetails(string videoUrl, int count)
+        // {
+        //     var http = new HttpClient();
+        //     var sut = new YouTubeVideo(http);
 
-            var result = await sut.ExtractVideoDetailsAsync(videoUrl).ConfigureAwait(false);
+        //     var result = await sut.ExtractVideoDetailsAsync(videoUrl).ConfigureAwait(false);
 
-            result.ShouldNotBeNull();
-            result.VideoId.ShouldNotBeNullOrWhiteSpace();
-            result.Title.ShouldNotBeNullOrWhiteSpace();
-            result.ShortDescription.ShouldNotBeNullOrWhiteSpace();
-            result.AvailableLanguageCodes.Count.ShouldBe(count);
-        }
+        //     result.ShouldNotBeNull();
+        //     result.VideoId.ShouldNotBeNullOrWhiteSpace();
+        //     result.Title.ShouldNotBeNullOrWhiteSpace();
+        //     result.ShortDescription.ShouldNotBeNullOrWhiteSpace();
+        //     result.AvailableLanguageCodes.Count.ShouldBe(count);
+        // }
      }
 }
